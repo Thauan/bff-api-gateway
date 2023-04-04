1741b3d7eb045ff342f178aa3a195dc0105127e2
Thauan Almeida <thaubr28@gmail.com>
2023-04-04 01:50:40 -0300

    feat: make bff

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Thauan/bff-api-gateway
 
 go 1.18
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 func init() {
 }
 
+type User struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
 func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/bff/v1/sign_in", func(w http.ResponseWriter, r *http.Request) {
+		reqBody, _ := ioutil.ReadAll(r.Body)
+
+		var user User
+		json.Unmarshal(reqBody, &user)
+		json.NewEncoder(w).Encode(user)
+
+		newData, err := json.Marshal(user)
+
+		println(newData)
+		println(err)
+
+		// Chama a camada de serviço e retorna uma resposta adequada.
+		// myData := User{Email: "teste", Password: "teste"}
+		json.NewEncoder(w).Encode(newData)
+	})
+
+	log.Fatal(http.ListenAndServe(":9090", r))
 }
